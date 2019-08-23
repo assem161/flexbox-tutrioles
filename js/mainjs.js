3213d1373673fb4546da5c6e62bd86b9c704e68b
assem161 <assem_mhu@yahoo.com>
2019-08-23 18:08:08 +0200

    enhance flex slider

--- js/mainjs.js
+++ js/mainjs.js
@@ -30,4 +30,88 @@ $(function(){
 		})
 	})
 
+
+	var sliderObj = {
+		move:0,
+		itemWidth : $('.contr2 .item').innerWidth(),
+		elemCount : $('.contr2 .item').length,
+		clicked: false,
+		current: 1
+	}
+
+
+            function nextFn(){
+                if(!sliderObj.clicked){
+
+					$('.arrow-r2').click(()=>{	
+                    	sliderObj.clicked = true;	
+                    	sliderObj.current = sliderObj.current+1;
+						$('.arrow-l2').removeClass('disabled')
+                    	console.log(sliderObj.current);					
+						var itemWidth = sliderObj.itemWidth;
+						sliderObj.move += itemWidth;
+						$('.contr2').css({
+							'transform':'translateX(-'+ sliderObj.move +'px)',
+							'transition': 'all 0.5s',
+						})
+
+						setTimeout(function(){
+							sliderObj.clicked = false;	
+						},1000)
+
+                        if($(window).width() > 600){
+		                    if(sliderObj.current> (sliderObj.elemCount)/2){
+		                        $('.arrow-r2').addClass('disabled');
+		                    	}else{
+		                         $('.arrow-r2').removeClass('disabled');
+		                    	}
+                        }else {
+		                    if(sliderObj.current> (sliderObj.elemCount-1)){
+		                        $('.arrow-r2').addClass('disabled');
+		                    	}else{
+		                         $('.arrow-r2').removeClass('disabled');
+		                   	   }
+                        }						
+								
+					})
+                }
+            }
+
+            nextFn();
+
+
+            function prevFn(){
+                if(!sliderObj.clicked){
+
+					$('.arrow-l2').click(()=>{	
+                    	sliderObj.clicked = true;	
+                    	sliderObj.current = sliderObj.current-1;
+
+						$('.arrow-r2').removeClass('disabled');                    						
+						var itemWidth = sliderObj.itemWidth;
+						sliderObj.move -= itemWidth;
+						$('.contr2').css({
+							'transform':'translateX(-'+ sliderObj.move +'px)',
+							'transition': 'all 0.5s',
+						})
+
+						setTimeout(function(){
+							sliderObj.clicked = false;	
+						},1000)
+
+                    if(sliderObj.current == 1){
+                        $('.arrow-l2').addClass('disabled');
+                    }else{
+                         $('.arrow-l2').removeClass('disabled');
+                    }						
+								
+					})						
+
+                }
+            }
+
+            prevFn();
+
+
+
 })
